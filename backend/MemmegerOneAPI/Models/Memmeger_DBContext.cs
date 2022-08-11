0a4a5fab556a04535514aeaf7ee8188b2d85db38
Wombyte <marcuspiele@web.de>
2022-08-11 14:08:26 +0200

    feat(backend): created db classes

---
+++ backend/MemmegerOneAPI/Models/Memmeger_DBContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MemmegerOneAPI.Models
+{
+    public partial class Memmeger_DBContext : DbContext
+    {
+        public Memmeger_DBContext()
+        {
+        }
+
+        public Memmeger_DBContext(DbContextOptions<Memmeger_DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TestTable> TestTables { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=memmeger.database.windows.net;Initial Catalog=Memmeger_DB;Persist Security Info=True;User ID=Mammeger;Password=Bremsnitz08?!");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestTable>(entity =>
+            {
+                entity.HasKey(e => e.NutzerId)
+                    .HasName("PK__TestTabl__C0768D35664E4BFC");
+
+                entity.ToTable("TestTable");
+
+                entity.Property(e => e.NutzerId)
+                    .HasMaxLength(500)
+                    .IsUnicode(false)
+                    .HasColumnName("NutzerID");
+
+                entity.Property(e => e.Benutzername)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserId)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("User_ID");
+
+                entity.Property(e => e.Username)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ backend/MemmegerOneAPI/Models/TestTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemmegerOneAPI.Models
+{
+    public partial class TestTable
+    {
+        public string? Benutzername { get; set; }
+        public string? Password { get; set; }
+        public string NutzerId { get; set; } = null!;
+    }
+}
